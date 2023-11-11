c43006bb429741e83dbe94e3622a949c9dea9d38
rizmyabdulla <rizmyabdulla@gmail.com>
2023-11-11 15:58:38 +0530

    PR01 - init

---
+++ js/animations.js
@@ -0,0 +1,43 @@
+// animation 1 - dynamic intro title
+const introTitle = document.getElementById("introTitle");
+let dynamicText = ["Steamed", "Fried", "Baked"];
+
+function playInAnimation() {
+  introTitle.style.animation = "intro-title-in 0.5s ease-in-out";
+  introTitle.innerHTML = dynamicText[0];
+  dynamicText.push(dynamicText.shift());
+}
+
+function playOutAnimation() {
+  introTitle.style.animation = "intro-title-out 0.5s ease-in-out";
+
+  introTitle.addEventListener("animationend", function () {
+    introTitle.removeEventListener("animationend", arguments.callee);
+    playInAnimation();
+  });
+}
+
+playInAnimation();
+
+setInterval(() => {
+  playOutAnimation();
+}, 4000);
+
+//animation 2 - Intro picture zooming and changing Animation
+
+const introImg = document.getElementById("introImg");
+let dynamicImg = ["food-img-01.jpg", "food-img-02.jpg", "food-img-03.jpg"];
+
+function playAnimation() {
+  introImg.src = `assets/${dynamicImg[0]}`;
+  dynamicImg.push(dynamicImg.shift());
+  introImg.style.animation = "intro-img-zoom 8s ease-in-out";
+  setTimeout(() => {
+    introImg.style.animation = "none";
+  }, 7900);
+}
+
+setInterval(playAnimation, 8000);
+
+playAnimation();
+
