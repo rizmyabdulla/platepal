b4b8d06286f8fd0bb93cbb67fa9d2e767a7759c4
rizmyabdulla <rizmyabdulla@gmail.com>
2023-11-13 22:28:44 +0530

    PR11 - smoothed intro img tansition

--- js/animations.js
+++ js/animations.js
@@ -29,13 +29,23 @@ const introImg = document.getElementById("introImg");
 let dynamicImg = ["food-img-01.jpg", "food-img-02.jpg", "food-img-03.jpg"];
 
 function playAnimation() {
-  introImg.src = `assets/images/${dynamicImg[0]}`;
-  dynamicImg.push(dynamicImg.shift());
+  introImg.style.opacity = "0"; // Set opacity to 0 before changing the source
+
+  setTimeout(() => {
+    introImg.src = `assets/images/${dynamicImg[0]}`;
+    dynamicImg.push(dynamicImg.shift());
+    introImg.style.opacity = "1"; // Set opacity back to 1 after changing the source
+  }, 500); // Adjust the delay to match the transition duration
+
   introImg.style.animation = "intro-img-zoom 8s ease-in-out";
+
+  setTimeout(() => {
+    introImg.style.opacity = "0";
+  }, 7650);
+
   setTimeout(() => {
     introImg.style.animation = "none";
-    introImg.src = `assets/images/${dynamicImg[0]}`;
-  }, 7900);
+  }, 7950);
 }
 
 setInterval(playAnimation, 8000);
